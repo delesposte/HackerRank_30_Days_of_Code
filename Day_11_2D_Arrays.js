81bb145f759f57865e41e78107a8a4be34eb4291
Flávio Delesposte <flaviodelesposte@hotmail.com>
2021-11-06 19:33:28 -0300

    Create Day_11_2D_Arrays.js

---
+++ Day_11_2D_Arrays.js
@@ -0,0 +1,43 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    let arr = Array(6);
+    for (let i = 0; i < 6; i++) {
+        arr[i] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+    console.log(getMaxHourglassSum(arr));
+}
+
+function getMaxHourglassSum(arr){
+    let max = 0;    
+    for (let i = 0; arr.length - i >= 3; i++) {
+        for (let j = 0; arr[i].length - j >= 3; j++) {
+            const sum = Number([arr[i][j]]) + Number([arr[i][j + 1]]) + 
+                Number([arr[i][j + 2]]) + Number([arr[i + 1][j + 1]]) + 
+                Number([arr[i + 2][j]]) + Number([arr[i + 2][j + 1]]) + 
+                Number([arr[i + 2][j + 2]]);
+            if(sum > max) max = sum;
+        }
+    }  
+    return max;       
+}
