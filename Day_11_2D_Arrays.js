ecd06e9a0afa7c89f6eebbb1e4ebf62be4320edf
Flávio Delesposte <flaviodelesposte@hotmail.com>
2021-11-06 19:38:22 -0300

    Update Day_11_2D_Arrays.js

--- Day_11_2D_Arrays.js
+++ Day_11_2D_Arrays.js
@@ -23,7 +23,10 @@ function readLine() {
 function main() {
     let arr = Array(6);
     for (let i = 0; i < 6; i++) {
-        arr[i] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+        arr[i] = readLine()
+            .replace(/\s+$/g, '')
+            .split(' ')
+            .map(arrTemp => parseInt(arrTemp, 10));
     }
     console.log(getMaxHourglassSum(arr));
 }
