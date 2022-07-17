3c0272927d6a1cee337ee573320a5d558c70b823
Flávio Delesposte <flaviodelesposte@hotmail.com>
2022-07-17 10:49:21 -0300

    Create Day_23_BST_Level_Order_Traversal.js

---
+++ Day_23_BST_Level_Order_Traversal.js
@@ -0,0 +1,74 @@
+function Node(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+};
+
+function BinarySearchTree() {
+    this.insert = function(root, data) {
+        if (root === null) {
+            this.root = new Node(data);
+            
+            return this.root;
+        }
+        
+        if (data <= root.data) {
+            if (root.left) {
+                this.insert(root.left, data);
+            } else {
+                root.left = new Node(data);
+            }
+        } else {
+            if (root.right) {
+                this.insert(root.right, data);
+            } else {
+                root.right = new Node(data);
+            }
+        }
+        
+        return this.root;
+    };
+
+    this.levelOrder = function(root) {
+      if(!root)
+        return;
+      
+      let result = '';
+      const queue = [root];
+      
+      while(queue.length > 0) {
+        const node = queue.shift();        
+        result = result + node.data + ' '; 
+        
+        if(node.left)
+          queue.push(node.left);     
+        
+        if(node.right)
+          queue.push(node.right);                  
+      }  
+      
+      process.stdout.write(result);
+	};
+};
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var _input = "";
+
+process.stdin.on('data', function (data) {
+    _input += data;
+});
+
+process.stdin.on('end', function () {
+    var tree = new BinarySearchTree();
+    var root = null;
+    
+    var values = _input.split('\n').map(Number);
+    
+    for (var i = 1; i < values.length; i++) {
+        root = tree.insert(root, values[i]);
+    }
+    
+    tree.levelOrder(root);
+});
